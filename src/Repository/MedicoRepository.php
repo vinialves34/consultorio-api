6cb9880f58776221dbd2518536a7c2894064a5ae
Vinicius <viniciusklonny@hotmail.com>
2020-09-06 16:16:53 -0300

    Crição de Repositório para Medicos

---
+++ src/Repository/MedicoRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Medico;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Medico|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Medico|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Medico[]    findAll()
+ * @method Medico[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MedicoRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Medico::class);
+    }
+}
